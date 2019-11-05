1e7711ffb366e9f42af5da861d9931341d17f3be
Godfrey Hendrix <samuelgodfreyhendrix@gmail.com>
2019-11-05 01:20:39 -0500

    Updated

--- Engine.py
+++ Engine.py
@@ -5,14 +5,13 @@ from lib import ClydeOBC
 from lib import ClydeSBandAntenna
 from lib import ClydeSunSensors
 from lib import FsatiUHFTransiever
-from lib import GomSpaceBP4
 from lib import ImperxC4180
 from lib import BlackflyColorCam
 from lib import ISISUHFAntenna
-from lib import P60PDU
-from lib import P60AC
 from lib import SolarPanels
 from lib import Tx2i
+from lib import XUA
+from lib import CSLBAT
 #I know this looks ridiculous but the * would not work
 
 test = True
@@ -76,14 +75,13 @@ def calculateComps(curLineMode, interval, curLineTime):
 	componentDraws.append(ClydeSBandAntenna.getPower(curLineMode, interval))
 	componentDraws.append(ClydeSunSensors.getPower(curLineMode, interval))
 	componentDraws.append(FsatiUHFTransiever.getPower(curLineMode, interval))
-	componentDraws.append(GomSpaceBP4.getPower(curLineMode, interval))
 	componentDraws.append(ImperxC4180.getPower(curLineMode, interval))
 	componentDraws.append(BlackflyColorCam.getPower(curLineMode, interval))
 	componentDraws.append(ISISUHFAntenna.getPower(curLineMode, interval))
-	componentDraws.append(P60PDU.getPower(curLineMode, interval))
-	componentDraws.append(P60AC.getPower(curLineMode, interval))
 	componentDraws.append(SolarPanels.getPower(curLineMode, interval))
 	componentDraws.append(Tx2i.getPower(curLineMode, interval))
+	componentDraws.append(XUA.getPower(curLineMode, interval))
+	componentDraws.append(CSLBAT.getPower(curLineMode, interval))
 	for x in range(len(componentDraws)):
 		total += componentDraws[x]
 
@@ -91,7 +89,7 @@ def calculateComps(curLineMode, interval, curLineTime):
 	return total, componentDraws
 
 def GenCsv(powerCoeff, outfile):
-	totalPower = 38
+	totalPower = 80
 	valid = True
 
 	#opens the timeline csv and scrapes the info from each line, processes, and writes to output.csv
@@ -111,7 +109,7 @@ def GenCsv(powerCoeff, outfile):
 		lineCount = 0 				#counts the number of lines of data the will be processed
 		totalPowerSum = totalPower	#holds the sum of the Battery Power for calculating the mean
 		maxBat = 0
-		minBat = 38
+		minBat = 81
 		minOccured = ""
 		maxOccured = ""
 		while nextLineTime != '':
@@ -121,7 +119,10 @@ def GenCsv(powerCoeff, outfile):
 
 			#this is a jerry rigged way to get the heater on the BP4 to turn on if in eclipse
 			if curLinePowGain <= 0:
-				powConsumed += (1.5	 * interval);
+				powConsumed += (1.6	 * interval)
+			#Comment out below after a 6U simulation is made
+			else:
+				curLinePowGain *= 1.25
 			#converts Joules/sec to joules over the given interval
 			actualPowGain = powerCoeff * (curLinePowGain * getInterval())
 			
@@ -133,9 +134,9 @@ def GenCsv(powerCoeff, outfile):
 			nextLineTime, nextLinePowGain, nextLineSol, nextLineMode = scrapeInfo(f.readline())
 
 			#leveling the battery power if the gain is over capacity
-			if totalPower > 38:
-				totalPower = 38
-				print("The power exceeded battery capacity and was leveled at time " + str(curLineTime))
+			if totalPower > 80:
+				totalPower = 80
+				# print("The power exceeded battery capacity and was leveled at time " + str(curLineTime))
 
 			#writing all the info the the output.csv
 			g.write(str(curLineTime) + "," + str(curLineMode) + "," + str(actualPowGain) + "," + str(powConsumed) + "," + str(totalPower) + ",")
@@ -178,7 +179,7 @@ def GenCsv(powerCoeff, outfile):
 if __name__ == "__main__":
 	while(True):
 		try:
-			userIn = input("Please enter the power coefficient (0,1] you would like to use. The default is 1")
+			userIn = input("Please enter the power coefficient (0,1] you would like to use. \n")
 			powerCoeff = float(userIn)
 			if powerCoeff <= 1 and powerCoeff > 0:
 				break
