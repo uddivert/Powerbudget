9eefce910b04e0e0320e6b134804a740aafeb1b3
Godfrey Hendrix <me@godfreyhendrix.com>
2020-11-18 09:52:18 -0500

    New simulation data

--- Inputs/genTimeLine.py
+++ Inputs/genTimeLine.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 previousTimeline = pd.read_csv("newTimeline.csv")
-newTimeline = pd.read_csv("ISS_25544_30sec.csv")
+newTimeline = pd.read_csv("MOCI_5PanelPower.csv")
 
 firstTime = newTimeline['Time (UTCG)'][0]
 
